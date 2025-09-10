5b16dd2fab2ad8a32195d8e4b947001aeeaba0f0
Byungjin Park <posquit0.bj@gmail.com>
2025-09-10 23:51:02 +0900

    feat: improve how to use resource_group in modules

--- modules/dns-firewall-domain-list/outputs.tf
+++ modules/dns-firewall-domain-list/outputs.tf
@@ -17,3 +17,19 @@ output "domains" {
   description = "The list of domains from the firewall domain list."
   value       = aws_route53_resolver_firewall_domain_list.this.domains
 }
+
+output "resource_group" {
+  description = "The resource group created to manage resources in this module."
+  value = merge(
+    {
+      enabled = var.resource_group.enabled && var.module_tags_enabled
+    },
+    (var.resource_group.enabled && var.module_tags_enabled
+      ? {
+        arn  = module.resource_group[0].arn
+        name = module.resource_group[0].name
+      }
+      : {}
+    )
+  )
+}
