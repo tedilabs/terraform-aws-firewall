1a3d467ac314678821b5a011fa8633eacbb99954
Byungjin Park (Claud) <posquit0.bj@gmail.com>
2026-09-02 16:30:27 +0900

    feat(dns-firewall-domain-list): support aws provider v6 (#58)

--- modules/dns-firewall-domain-list/main.tf
+++ modules/dns-firewall-domain-list/main.tf
@@ -20,6 +20,8 @@ locals {
 ###################################################
 
 resource "aws_route53_resolver_firewall_domain_list" "this" {
+  region = var.region
+
   name    = var.name
   domains = var.domains
 
