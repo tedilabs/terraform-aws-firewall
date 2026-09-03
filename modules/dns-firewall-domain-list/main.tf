e72577125925c7da52b4757ffe40ce9d207f4e64
Byungjin Park <posquit0.bj@gmail.com>
2026-09-04 01:25:48 +0900

    feat(dns-firewall-domain-list): support aws managed domain lists

--- modules/dns-firewall-domain-list/main.tf
+++ modules/dns-firewall-domain-list/main.tf
@@ -15,11 +15,22 @@ locals {
 }
 
 
+locals {
+  managed_domain_lists = yamldecode(file("${path.module}/managed-domain-lists.yaml"))
+  managed_domain_list_id = try(
+    local.managed_domain_lists[one(data.aws_region.this[*].region)][var.name],
+    null,
+  )
+}
+
+
 ###################################################
-# Domain List for DNS Firewall
+# Custom Domain List for DNS Firewall
 ###################################################
 
 resource "aws_route53_resolver_firewall_domain_list" "this" {
+  count = var.type == "CUSTOM" ? 1 : 0
+
   region = var.region
 
   name    = var.name
@@ -31,5 +42,53 @@ resource "aws_route53_resolver_firewall_domain_list" "this" {
     },
     local.module_tags,
     var.tags,
+  )
+}
+
+
+###################################################
+# Managed Domain List for DNS Firewall
+###################################################
+
+data "aws_region" "this" {
+  count = var.type == "MANAGED" ? 1 : 0
+
+  region = var.region
+}
+
+data "aws_route53_resolver_firewall_domain_list" "this" {
+  count = var.type == "MANAGED" ? 1 : 0
+
+  region = var.region
+
+  firewall_domain_list_id = local.managed_domain_list_id
+
+  lifecycle {
+    precondition {
+      condition     = local.managed_domain_list_id != null
+      error_message = "The `name` should be the name of an AWS managed domain list which is available in the region. Run `get-managed-domain-lists.sh` to refresh `managed-domain-lists.yaml` if the domain list is missing from it."
+    }
+  }
+}
+
+
+locals {
+  domain_list = (var.type == "CUSTOM"
+    ? {
+      region       = one(aws_route53_resolver_firewall_domain_list.this[*].region)
+      arn          = one(aws_route53_resolver_firewall_domain_list.this[*].arn)
+      id           = one(aws_route53_resolver_firewall_domain_list.this[*].id)
+      name         = one(aws_route53_resolver_firewall_domain_list.this[*].name)
+      domains      = one(aws_route53_resolver_firewall_domain_list.this[*].domains)
+      domain_count = length(one(aws_route53_resolver_firewall_domain_list.this[*].domains))
+    }
+    : {
+      region       = one(data.aws_route53_resolver_firewall_domain_list.this[*].region)
+      arn          = one(data.aws_route53_resolver_firewall_domain_list.this[*].arn)
+      id           = one(data.aws_route53_resolver_firewall_domain_list.this[*].firewall_domain_list_id)
+      name         = one(data.aws_route53_resolver_firewall_domain_list.this[*].name)
+      domains      = toset([])
+      domain_count = one(data.aws_route53_resolver_firewall_domain_list.this[*].domain_count)
+    }
   )
 }
